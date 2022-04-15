40047ea1355156c858394a8ca3c69dd44e1a7100
Magdiel <mgomez2091@gmail.com>
2022-04-15 13:13:49 -0600

    Change logo

--- src/components/Navb/Navbar.tsx
+++ src/components/Navb/Navbar.tsx
@@ -1,21 +1,21 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import { MouseEvent, useState } from 'react'
 import MenuIcon from '@mui/icons-material/Menu'
-import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import IconButton from '@mui/material/IconButton'
 import Toolbar from '@mui/material/Toolbar'
 import { routes } from '../../router'
 import DrawerMenu from '../DrawerMenu'
-import { Text } from './styles'
+import { Navbar as AppBar, Text } from './styles'
 
 export const Navbar = () => {
   const [isOpen, onClose] = useState(false)
 
   return (
     <>
-      <AppBar position="static">
+      <AppBar color="primary" position="static">
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             <Link href="/">
@@ -24,7 +24,7 @@ export const Navbar = () => {
                 noWrap
                 sx={{ display: { xs: 'none', md: 'flex' } }}
               >
-                LOGO
+                <Image src="/img/logo_dooper.svg" width={100} height={40} />
               </Text>
             </Link>
             <Box>
@@ -41,7 +41,7 @@ export const Navbar = () => {
                 noWrap
                 sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
               >
-                LOGO
+                <Image src="/img/logo_dooper.svg" width={100} height={40} />
               </Text>
             </Link>
           </Toolbar>
--- src/components/Navb/styles.tsx
+++ src/components/Navb/styles.tsx
@@ -1,8 +1,11 @@
-import { Typography, styled } from '@mui/material'
+import { AppBar, Typography, styled } from '@mui/material'
 
 export const Text = styled(Typography)`
   color: ${(props) => props.theme.palette.primary.light};
   align-items: center;
   justify-content: center;
   font-size: ${(props) => props.theme.typography.h4.fontSize};
+`
+export const Navbar = styled(AppBar)`
+  background-color: #2b3445;
 `
